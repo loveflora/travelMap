a8cabef3c358312419be3cafd04a44ce79fbb3ab
sehwa <love7620640@naver.com>
2023-08-07 02:16:51 +0900

    <feat> video

--- index.js
+++ index.js
@@ -12,8 +12,9 @@ const swiper = new Swiper(".swiper-container", {
   loopAdditionalSlides: 1,
 });
 
-//] asia 차트
-const ctx = document.getElementById("asiaChart");
+//] 차트
+
+const asiaCtx = document.getElementById("asiaChart");
 
 const data = {
   labels: [
@@ -27,16 +28,16 @@ const data = {
   ],
   datasets: [
     {
-      label: "2023년 국민 해외관광객",
+      label: "2023년 국민 해외관광객 (Asia)",
       data: [3128470, 4346567, 1602183, 538766, 673841, 112371, 265074],
       backgroundColor: [
         "rgb(255, 99, 132)",
         "rgb(255, 205, 86)",
         "rgb(201, 203, 207)",
-        "rgb(54, 162, 235)",
-        "rgb(54, 162, 235)",
-        "rgb(54, 162, 235)",
-        "rgb(54, 162, 235)",
+        "rgb(114, 212, 125)",
+        "rgb(54, 116, 135)",
+        "rgb(54, 162, 25)",
+        "rgb(74, 162, 235)",
       ],
     },
   ],
@@ -50,19 +51,38 @@ const config = {
   },
 };
 
-new Chart(ctx, config);
-
-//]
+new Chart(asiaCtx, config);
 
 //++ aisa 차트 생성 함수
 const chartWrapper = document.querySelector(".chartWrapper");
 const asia = document.querySelector(".asia");
+// const europe = document.querySelector(".europe");
+// const america = document.querySelector(".america");
+// const oceania = document.querySelector(".oceania");
+// const africa = document.querySelector(".africa");
+
+const continentList = ["asia", "europe", "america", "oceania", "africa"];
 
 function showChartHandler() {
   chartWrapper.classList.toggle("show");
 }
 
-// title.style.pointerEvents = "none";
-
-// asia 클릭
 asia.addEventListener("click", showChartHandler);
+
+for (let i = 1; i <= continentList.length; i++) {
+  document
+    .querySelector(`.${continentList[i]}`)
+    .addEventListener("click", () => {
+      document
+        .getElementById(`${continentList[i]}Iframe`)
+        .classList.toggle("show");
+    });
+}
+
+// europe.addEventListener("click", () => {
+//   document.getElementById("europeIframe").classList.toggle("show");
+// });
+
+// continentList[2].addEventListener("click", () => {
+//   document.getElementById(`${continentList[2]}Iframe`).classList.toggle("show");
+// });
